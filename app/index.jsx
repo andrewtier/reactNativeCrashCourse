88bd678d0a054626793c88f0a8db4ea5ae9eeb35
andrewtier <132238552+andrewtier@users.noreply.github.com>
2024-08-16 19:15:19 +1200

    aora! v0.1

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hotspot I must</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Aora!</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: 'blue' }}>Skibidi bop bop bop</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
